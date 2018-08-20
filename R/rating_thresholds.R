357abf315eeb4a0e9ffe6de639e9b249ae5f627c
zsmith27 <zsmith@icprb.org>
2018-08-20 13:50:53 -0400

    Commented out becuase these scripts require specific directories and files.

--- R/rating_thresholds.R
+++ R/rating_thresholds.R
@@ -1,31 +1,31 @@
-
-library(readxl)
-file.dir <- "H:/Projects/Chessie_BIBI/report/FINAL_May25_2017/2017_Data/Scores_Ratings/BIBI_Scores_Ratings_06292017.xlsx"
-rating.list <- lapply(excel_sheets(file.dir)[-1], function(sheet.i) {
-  read_excel(sheet.i, path = file.dir) %>% 
-    select(SPATIAL, HALF_REF_10, REF_10, REF_25, REF_50) %>% 
-    distinct() %>% 
-    mutate(res = sheet.i)
-})
-
-rating.thresh <- bind_rows(rating.list) %>% 
-  clean_df() %>% 
-  mutate(
-    spatial_resolution = case_when(
-    stringr::str_detect(res, "ches") ~ "chesapeake_wide",
-    stringr::str_detect(res, "bioregion") ~ "bioregion",
-    stringr::str_detect(res, "region") ~ "region",
-    TRUE ~ "ERROR"
-  ),
-  taxonomic_resolution = case_when(
-    stringr::str_detect(res, "order") ~ "order",
-    stringr::str_detect(res, "family") ~ "family",
-    stringr::str_detect(res, "genus") ~ "genus",
-    TRUE ~ "ERROR"
-  )
-  ) %>% 
-  select(-spatial_resolution, -res) %>% 
-  rename(spatial_resolution = spatial) %>% 
-  select(spatial_resolution, taxonomic_resolution, everything())
-
-data.table::fwrite(rating.thresh, "rating_threshold_06292017.csv")
+# 
+# library(readxl)
+# file.dir <- "H:/Projects/Chessie_BIBI/report/FINAL_May25_2017/2017_Data/Scores_Ratings/BIBI_Scores_Ratings_06292017.xlsx"
+# rating.list <- lapply(excel_sheets(file.dir)[-1], function(sheet.i) {
+#   read_excel(sheet.i, path = file.dir) %>% 
+#     select(SPATIAL, HALF_REF_10, REF_10, REF_25, REF_50) %>% 
+#     distinct() %>% 
+#     mutate(res = sheet.i)
+# })
+# 
+# rating.thresh <- bind_rows(rating.list) %>% 
+#   clean_df() %>% 
+#   mutate(
+#     spatial_resolution = case_when(
+#     stringr::str_detect(res, "ches") ~ "chesapeake_wide",
+#     stringr::str_detect(res, "bioregion") ~ "bioregion",
+#     stringr::str_detect(res, "region") ~ "region",
+#     TRUE ~ "ERROR"
+#   ),
+#   taxonomic_resolution = case_when(
+#     stringr::str_detect(res, "order") ~ "order",
+#     stringr::str_detect(res, "family") ~ "family",
+#     stringr::str_detect(res, "genus") ~ "genus",
+#     TRUE ~ "ERROR"
+#   )
+#   ) %>% 
+#   select(-spatial_resolution, -res) %>% 
+#   rename(spatial_resolution = spatial) %>% 
+#   select(spatial_resolution, taxonomic_resolution, everything())
+# 
+# data.table::fwrite(rating.thresh, "rating_threshold_06292017.csv")
